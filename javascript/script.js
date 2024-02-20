a34598753f1071cb2089b8009e3afd4e7c0a8404
Dev Bro <devbro.js@gmail.com>
2024-02-20 20:57:37 +0100

    modal functionality

--- javascript/script.js
+++ javascript/script.js
@@ -4,17 +4,52 @@ const signUpBtn = document.getElementById("signupbtn");
 const signInBtn = document.getElementById("signinbtn");
 const nameInput = document.getElementById("nameinput");
 const newTitle = document.getElementById("newtitle");
+const hideOnSignIn = document.querySelectorAll(".hide-on-signIn");
 
-signinbtn.onclick = function () {
-  nameinput.style.maxHeight = "0";
-  newtitle.innerHTML = "Sign In";
-  signupbtn.classList.add("sign");
-  signinbtn.classList.remove("sign");
+const updateFormUI = function (message) {
+  signUpBtn.classList.toggle("active");
+  signUpBtn.classList.toggle("normal");
+  newTitle.textContent = `${message}`;
+  signInBtn.classList.toggle("active");
+  signInBtn.classList.toggle("normal");
 };
 
-signupbtn.onclick = function () {
-  nameinput.style.maxHeight = "60px";
-  newtitle.innerHTML = "Sign Up";
-  signupbtn.classList.remove("sign");
-  signinbtn.classList.add("sign");
-};
+signInBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  hideOnSignIn.forEach((input) => {
+    input.classList.add("hidden");
+  });
+
+  updateFormUI("Sign In");
+});
+
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  hideOnSignIn.forEach((input) => {
+    input.classList.remove("hidden");
+  });
+
+  updateFormUI("Sign Up");
+});
+
+const navbarIconContainer = document.querySelector(".navbar-icon");
+const formContainer = document.querySelector(".container");
+
+navbarIconContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".user-icon");
+
+  if (!clicked) return;
+  formContainer.classList.toggle("hidden");
+});
+
+//Close Form Modal
+
+const btnCloseModal = document.querySelector(".close-form");
+
+btnCloseModal.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  formContainer.classList.toggle("hidden");
+});
