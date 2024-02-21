6f0439279cc91c6ff9bcf215b366d4b7e032d989
Dev Bro <devbro.js@gmail.com>
2024-02-21 12:28:43 +0100

    update navbar on click of cart icon

--- javascript/script.js
+++ javascript/script.js
@@ -12,6 +12,48 @@ const wrapper = document.getElementById("wrapper");
 // const anchorTags = document.querySelectorAll("a");
 
 function app() {
+  const updateNavbar = function () {
+    wrapper.innerHTML = "";
+
+    const html = `
+    <nav class="navbar navbar-color-2">
+    <div class="navbar__col-1">
+      <h1 class="navbar__heading">
+        <a href="./" id="innerLuxeLogo">InnerLuxe</a>
+      </h1>
+    </div>
+
+    <div class="navbar__col-2">
+      <ul class="navbar__list">
+        <li class="navbar__list-items">
+          <a href="#" class="">SHOP NOW</a>
+        </li>
+        <li class="navbar__list-items">
+          <a href="#" class="">COLLECTION</a>
+        </li>
+        <li class="navbar__list-items">
+          <a href="#" class="">INSPIRATION</a>
+        </li>
+        <li class="navbar__list-items">
+          <a href="#" class="">CONTACT</a>
+        </li>
+      </ul>
+
+      <div class="navbar-icon">
+        <a href="#"><i class="ri-search-2-line"></i></a>
+        <a href="#cart"
+          ><i class="ri-shopping-cart-2-line cart-icon"></i
+        ></a>
+        <a href="#"><i class="ri-user-line user-icon"></i></a>
+      </div>
+    </div>
+  </nav>
+    
+    `;
+
+    wrapper.insertAdjacentHTML("beforeend", html);
+  };
+
   const updateFormUI = function (message) {
     signUpBtn.classList.toggle("active");
     signUpBtn.classList.toggle("normal");
@@ -41,20 +83,14 @@ function app() {
   });
 
   navbarIconContainer.addEventListener("click", (e) => {
+    e.preventDefault();
     const clickedUser = e.target.closest(".user-icon");
     const cart = e.target.closest(".cart-icon");
 
     if (clickedUser) {
       formContainer.classList.toggle("hidden");
     } else if (cart) {
-      wrapper.innerHTML = "";
-
-      const headerEl = document.createElement("header");
-      headerEl.style.backgroundColor = "red";
-
-      headerEl.classList.add("header");
-      headerEl.classList.add("header-2");
-      wrapper.appendChild(headerEl);
+      updateNavbar();
     }
   });
 
