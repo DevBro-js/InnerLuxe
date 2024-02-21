a89b20810e7085c19969f61be778c27ec199e7fa
Dev Bro <devbro.js@gmail.com>
2024-02-21 12:11:10 +0100

    add eventlistner to cart icon

--- javascript/script.js
+++ javascript/script.js
@@ -5,51 +5,70 @@ const signInBtn = document.getElementById("signinbtn");
 const nameInput = document.getElementById("nameinput");
 const newTitle = document.getElementById("newtitle");
 const hideOnSignIn = document.querySelectorAll(".hide-on-signIn");
-
-const updateFormUI = function (message) {
-  signUpBtn.classList.toggle("active");
-  signUpBtn.classList.toggle("normal");
-  newTitle.textContent = `${message}`;
-  signInBtn.classList.toggle("active");
-  signInBtn.classList.toggle("normal");
-};
-
-signInBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  hideOnSignIn.forEach((input) => {
-    input.classList.add("hidden");
-  });
-
-  updateFormUI("Sign In");
-});
-
-signUpBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  hideOnSignIn.forEach((input) => {
-    input.classList.remove("hidden");
-  });
-
-  updateFormUI("Sign Up");
-});
-
 const navbarIconContainer = document.querySelector(".navbar-icon");
 const formContainer = document.querySelector(".container");
+const wrapper = document.getElementById("wrapper");
 
-navbarIconContainer.addEventListener("click", (e) => {
-  const clicked = e.target.closest(".user-icon");
+// const anchorTags = document.querySelectorAll("a");
 
-  if (!clicked) return;
-  formContainer.classList.toggle("hidden");
-});
+function app() {
+  const updateFormUI = function (message) {
+    signUpBtn.classList.toggle("active");
+    signUpBtn.classList.toggle("normal");
+    newTitle.textContent = `${message}`;
+    signInBtn.classList.toggle("active");
+    signInBtn.classList.toggle("normal");
+  };
 
-//Close Form Modal
+  signInBtn.addEventListener("click", (e) => {
+    e.preventDefault();
 
-const btnCloseModal = document.querySelector(".close-form");
+    hideOnSignIn.forEach((input) => {
+      input.classList.add("hidden");
+    });
 
-btnCloseModal.addEventListener("click", (e) => {
-  e.preventDefault();
+    updateFormUI("Sign In");
+  });
 
-  formContainer.classList.toggle("hidden");
-});
+  signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    hideOnSignIn.forEach((input) => {
+      input.classList.remove("hidden");
+    });
+
+    updateFormUI("Sign Up");
+  });
+
+  navbarIconContainer.addEventListener("click", (e) => {
+    const clickedUser = e.target.closest(".user-icon");
+    const cart = e.target.closest(".cart-icon");
+
+    if (clickedUser) {
+      formContainer.classList.toggle("hidden");
+    } else if (cart) {
+      wrapper.innerHTML = "";
+
+      const headerEl = document.createElement("header");
+      headerEl.style.backgroundColor = "red";
+
+      headerEl.classList.add("header");
+      headerEl.classList.add("header-2");
+      wrapper.appendChild(headerEl);
+    }
+  });
+
+  //Close Form Modal
+
+  const btnCloseModal = document.querySelector(".close-form");
+
+  btnCloseModal.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    formContainer.classList.toggle("hidden");
+  });
+
+  //cart section
+}
+
+app();
